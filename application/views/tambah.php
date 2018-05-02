48abe202c756b3c7d6b0ef8bd31d376d84eafabb
Lilianadiania <lilianadiania16@gmail.com>
2018-05-02 13:08:39 +0700

    yes

---
+++ application/views/tambah.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="http://localhost/Codeigniter/index.php/User/assets/paytic.css">
+</head>
+<body>
+	<div id="border">
+		<center>
+			<form action="http://localhost/Codeigniter/index.php/User/masukkan" method="post">
+
+			<input type="text" name="username" placeholder="username"><br>
+			<input type="text" name="password" placeholder="password"><br>
+			<input type="text" name="fullname" placeholder="fullname"><br>
+			<input type="text" name="level" placeholder="level"><br>
+			<input type="submit" value="save">
+			</form>
+		</center>
+	</div>
+</body>
+</html>
